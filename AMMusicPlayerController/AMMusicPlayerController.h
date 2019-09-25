aac26380688c30701c4c63319a04113bf121d2ff
yoheimuta <yoheimuta@gmail.com>
2019-09-25 16:48:15 +0900

    init

---
+++ AMMusicPlayerController/AMMusicPlayerController.h
@@ -0,0 +1,19 @@
+//
+//  AMMusicPlayerController.h
+//  AMMusicPlayerController
+//
+//  Created by YOSHIMUTA YOHEI on 2019/09/25.
+//  Copyright © 2019 YOSHIMUTA YOHEI. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for AMMusicPlayerController.
+FOUNDATION_EXPORT double AMMusicPlayerControllerVersionNumber;
+
+//! Project version string for AMMusicPlayerController.
+FOUNDATION_EXPORT const unsigned char AMMusicPlayerControllerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AMMusicPlayerController/PublicHeader.h>
+
+
