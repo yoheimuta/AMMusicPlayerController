4d989eb3a188e8f7a41763a46b8a57a32b9c5135
yoheimuta <yoheimuta@gmail.com>
2019-09-26 19:15:36 +0900

    feat: Table demo

--- Demo/Demo/ViewController.swift
+++ Demo/Demo/ViewController.swift
@@ -11,29 +11,26 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    var presentTableControllerButton = UIButton(type: UIButton.ButtonType.system)
+    let tableView = UITableView()
+    let urls = [
+        "https://storage.googleapis.com/maison-great-dev/oss/musicplayer/tagmp3_1473200_1.mp3",
+        "https://storage.googleapis.com/maison-great-dev/oss/musicplayer/tagmp3_2160166.mp3",
+        "https://storage.googleapis.com/maison-great-dev/oss/musicplayer/tagmp3_4690995.mp3",
+        "https://storage.googleapis.com/maison-great-dev/oss/musicplayer/tagmp3_9179181.mp3",
+    ].map { URL(string: $0)! }
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        presentTableControllerButton.setTitle("Show TableController", for: .normal)
-        presentTableControllerButton.addTarget(self,
-                                               action: #selector(presentModalViewController),
-                                               for: .touchUpInside)
-        presentTableControllerButton.sizeToFit()
-        presentTableControllerButton.center.x = view.frame.width / 2
-        presentTableControllerButton.center.y = view.frame.height / 4 * 3
-        view.addSubview(presentTableControllerButton)
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "Cell")
+        view.addSubview(tableView)
+
+        tableView.frame = CGRect(x: 0, y: 0, width: view.frame.size.width, height: view.frame.size.height)
     }
 
-    @objc func presentModalViewController() {
-        let urls = [
-            "https://storage.googleapis.com/maison-great-dev/oss/musicplayer/tagmp3_1473200_1.mp3",
-            "https://storage.googleapis.com/maison-great-dev/oss/musicplayer/tagmp3_2160166.mp3",
-            "https://storage.googleapis.com/maison-great-dev/oss/musicplayer/tagmp3_4690995.mp3",
-            "https://storage.googleapis.com/maison-great-dev/oss/musicplayer/tagmp3_9179181.mp3",
-        ].map { URL(string: $0)! }
-
+    func presentModalViewController(index: Int) {
         let config = AMMusicPlayerConfig(lyricsLabel: "Words",
                                          confirmConfig: AMMusicPlayerConfig.ConfirmConfig(
                                              needConfirm: true,
@@ -42,7 +39,9 @@ class ViewController: UIViewController {
                                              confirmActionTitle: "OK",
                                              cancelActionTitle: "No"))
 
-        let modal = AMMusicPlayerController.make(urls: urls, config: config)
+        let modal = AMMusicPlayerController.make(urls: urls,
+                                                 index: index,
+                                                 config: config)
         modal.delegate = self
         modal.presentPlayer(src: self)
     }
@@ -55,5 +54,25 @@ extension ViewController: AMMusicPlayerDelegate {
 
     func musicPlayerControllerDidDismissBySwipe() {
         print("DismissBySwipe")
+    }
+}
+
+extension ViewController: UITableViewDataSource {
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        cell.textLabel?.text = "Song \(indexPath.row + 1)"
+        cell.transform = .identity
+        return cell
+    }
+
+    func tableView(_: UITableView, numberOfRowsInSection _: Int) -> Int {
+        return urls.count
+    }
+}
+
+extension ViewController: UITableViewDelegate {
+    func tableView(_: UITableView, didSelectRowAt ip: IndexPath) {
+        presentModalViewController(index: ip.row)
     }
 }
